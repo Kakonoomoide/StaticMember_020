bd9f9a0d10ce50c06c03bb66196d40f1d1106d8c
kizaru <rizkiramada128@gmail.com>
2023-05-17 17:20:17 +0700

    memangil chain func yan gtelajh di buat

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -2,9 +2,25 @@
 using namespace std;
 
 class buku {
+    string judul;
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this; //chain func
+    }
 
-};
+    string getJudul() {
+        return this->judul;
+    }
+} bukunya;
 
 int main(){
+    //normal
+    //bukunya.setJudul("matematika");
+    //cout << bukunya.getJudul();
     
+    //sederhana
+    cout << bukunya.setJudul("matematika").getJudul(); //chain func
+    
+    return 0;
 }
